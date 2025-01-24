a3cb00ca8515d77dbc9f73c49db8134a7e339822
Tales Vieira <devtalesvieira@gmail.com>
2025-01-24 14:42:29 -0300

    Persistindo produtos no banco de dados.

--- src/main/java/io/github/cursodsousa/produtosapi/controller/ProdutoController.java
+++ src/main/java/io/github/cursodsousa/produtosapi/controller/ProdutoController.java
@@ -1,18 +1,31 @@
 package io.github.cursodsousa.produtosapi.controller;
 
 import io.github.cursodsousa.produtosapi.model.Produto;
+import io.github.cursodsousa.produtosapi.repository.ProdutoRepository;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.UUID;
+
 @RestController //diz ao spring que essa classe vai receber requisições rest
 @RequestMapping ("produtos")// vai dizer qual é a url base desse controller
 public class ProdutoController {
 
+
+    private ProdutoRepository produtoRepository;
+
+    public ProdutoController(ProdutoRepository produtoRepository) {
+        this.produtoRepository = produtoRepository;
+    }
+
     @PostMapping //quando vou salvar / criar um recurso no servidor
     public Produto salvar(@RequestBody Produto produto){
         System.out.println("Produto recebido: " + produto);
+        var id = UUID.randomUUID().toString();
+        produto.setId(id);
+        produtoRepository.save(produto);
         return produto;
     }
 
