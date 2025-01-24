9d622f6e51de8cddae8f9387d2370b45666117f3
Tales Vieira <devtalesvieira@gmail.com>
2025-01-24 14:57:53 -0300

    Obtendo os dados do produto.

--- src/main/java/io/github/cursodsousa/produtosapi/controller/ProdutoController.java
+++ src/main/java/io/github/cursodsousa/produtosapi/controller/ProdutoController.java
@@ -2,11 +2,9 @@ package io.github.cursodsousa.produtosapi.controller;
 
 import io.github.cursodsousa.produtosapi.model.Produto;
 import io.github.cursodsousa.produtosapi.repository.ProdutoRepository;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @RestController //diz ao spring que essa classe vai receber requisições rest
@@ -23,11 +21,22 @@ public class ProdutoController {
     @PostMapping //quando vou salvar / criar um recurso no servidor
     public Produto salvar(@RequestBody Produto produto){
         System.out.println("Produto recebido: " + produto);
+
         var id = UUID.randomUUID().toString();
         produto.setId(id);
+
+
         produtoRepository.save(produto);
         return produto;
     }
+    @GetMapping("{id}")
+    public Produto obterPorId(@PathVariable("id") String id){
+       // Optional<Produto> produto = produtoRepository.findById(id);
+        //return produto.isPresent() ? produto.get() : null;
 
+        //o código de baixo faz a mesma coisa que os dois de cima
 
+        return produtoRepository.findById(id).orElse(null);
+    }
+    
 }
